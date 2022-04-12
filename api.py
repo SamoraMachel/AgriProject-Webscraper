15aba94f2a47c7558fb4a9912e30bd4dcec8a9b3
Samora Machel <samora@me.laptop>
2022-04-12 18:37:50 +0300

    added detail page

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
+from crypt import methods
 from random import Random
 from flask import jsonify, request
 import time
-from scraper import scrape_with_crochet, output_data
+from scraper import scrape_with_crochet, output_data, AgroSpider, AgroDetailSpider
 from config import app, crochet
 
 
@@ -9,12 +10,17 @@ from config import app, crochet
 @app.route("/", methods=['GET'])
 def home():
     return jsonify({
-        "/recommend/" : "gets general data users display",
+        "/recommend/" : "get random recommendations to display in the home screen",
         "/question/" : {    
-            "architecture" : {
-                "search" : "data to be searched"
+            "POST" : {
+                "data" : "data to be searched"
             }
-        }
+        },
+        "/detail/" : {
+            "POST" : {
+                "data" : "detail page to be scraped"
+            }
+        },
     })
 
 @crochet.run_in_reactor
@@ -34,16 +40,23 @@ def requestForData():
         'soil health',
         'poultry health'
     ] 
-    scrape_with_crochet(recommendation_list[Random().randint(0, recommendation_list.__len__() - 1)])
+    scrape_with_crochet(recommendation_list[Random().randint(0, recommendation_list.__len__() - 1)], AgroSpider)
     time.sleep(5)
     return jsonify(output_data)
 
 @app.route('/question/', methods=['POST'])
 def scrapeData():
     data  = dict(request.get_json())
-    search_data = data.get('search')
-    scrape_with_crochet(search_data)
+    search_data = data.get('data')
+    scrape_with_crochet(search_data, AgroSpider)
     time.sleep(5)
     return jsonify(output_data)
-    
+
+@app.route('/detail/', methods=['POST'])
+def detail(): 
+    data  = dict(request.get_json())
+    link = data.get('data')
+    scrape_with_crochet(link, AgroDetailSpider)
+    time.sleep(3)
+    return jsonify(output_data)
 
--- scraper.py
+++ scraper.py
@@ -23,34 +23,62 @@ class AgroSpider(scrapy.Spider):
     def __init__(self, name=None, **kwargs):
         super().__init__(name, **kwargs)
         self.start_urls = {
-            question_search(kwargs.get("search", "goat feed"))
+            question_search(kwargs.get("data", "goat feed"))
         }
 
     
     def parse(self, response, **kwargs):
         for article in response.css(".public-search-result"):
+            
+            # the initial methods brings a null cause not all the text are in the paragraph tag
+            scraped_text = article.css(".content .field-body p::text").get()
+            if not scraped_text:
+                scraped_text = article.css(".content .field-body::text").get().strip('\n').strip(' ')
+                
             yield {
                 "image" : article.css(".field-image picture img::attr(data-srcset)").get(),
                 "title" : article.css(".content h2 a::text").get(),
-                "text" : article.css(".content .field-body p::text").get() if not None else article.css(".content .field-body::text").get().strip('\n').strip(' '),
+                "text" : scraped_text, 
                 "author": article.css(".content .content-footer .field-byline a::text").get(), 
                 "link": article.css(".content h2 a::attr(href)").get()
             }
         
         
         return super().parse(response, **kwargs)
+    
+
+class AgroDetailSpider(scrapy.Spider):
+    name = "detail_spider"
+    
+    def __init__(self, name=None, **kwargs):
+        super().__init__(name, **kwargs)
+        self.start_urls = {
+            kwargs.get("data", None)
+        }
+    
+    def parse(self, response, **kwargs):
+        yield {
+            "title" : response.css(".page-primary-content h1::text").get(),
+            "author" : response.css('.page-primary-content .field-byline a::text').get(),
+            "date" : response.css('.page-primary-content .byline-date::text').get(),
+            "text" : response.css("section article .field-body p::text").getall()
+        }
+        
+        return super().parse(response, **kwargs)
+        
+
 
 def _crawler_result(item, response, spider):
     output_data.append(dict(item)) 
 
 @crochet.run_in_reactor
-def scrape_with_crochet(search_str : str):
+def scrape_with_crochet(search_str : str, spider : scrapy.Spider):
     output_data.clear()
     # This will connect to the dispatcher that will kind of loop the code between these two functions.
     dispatcher.connect(_crawler_result, signal=signals.item_scraped)
     
     # This will connect to the ReviewspiderSpider function in our scrapy file and after each yield will pass to the crawler_result function.
-    eventual = CrawlerRunner().crawl(AgroSpider, search = search_str)
+    eventual = CrawlerRunner().crawl(spider, data = search_str)
     return eventual
 
 
