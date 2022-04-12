c3b9d9d2f35cc94d67a5aa050509cc35c0da9308
Samora Machel <samora@me.laptop>
2022-04-12 15:28:21 +0300

    Added requirements file

---
+++ main.py
@@ -0,0 +1,11 @@
+from config import app
+from api import *
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# uncomment to run the project locally
+# if __name__ == '__main__':
+    # app.run(debug=os.getenv("DEBUG"), host='0.0.0.0', port=5500)
